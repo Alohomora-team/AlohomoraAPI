e452109b0e250a09a32517b412f9bf76d2f3cf9f
luiscesm <luiscesm1@gmail.com>
2019-09-13 11:21:10 -0300

    Adicionado a proteção JWT para a API

--- app/portariaVirtual/accounts/views.py
+++ app/portariaVirtual/accounts/views.py
@@ -6,8 +6,13 @@ from django.http import HttpResponse
 # rest framework imports
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated 
 
 class HelloView(APIView):
+
+    #verify into header if token is valid
+    permission_classes = (IsAuthenticated,) 
+
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
