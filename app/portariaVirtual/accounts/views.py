e95a7bde7edcee6f6665bf231f435de6dedf91c8
luiscesm <luiscesm1@gmail.com>
2019-09-13 11:18:43 -0300

    Criação da API View HelloView para teste e sua respectiva rota

--- app/portariaVirtual/accounts/views.py
+++ app/portariaVirtual/accounts/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# rest framework imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class HelloView(APIView):
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
--- app/portariaVirtual/urls.py
+++ app/portariaVirtual/urls.py
@@ -4,7 +4,10 @@ from django.urls import include, path
 from graphene_django.views import GraphQLView
 from portariaVirtual.schema import schema
 
+from portariaVirtual.accounts import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', GraphQLView.as_view(schema=schema, graphiql=True)),
+    path('hello/', views.HelloView.as_view(), name='hello'),
 ]
