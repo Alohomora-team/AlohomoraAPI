5879466a95a6a61494f1793f4236b199e37691f7
luiscesm <luiscesm1@gmail.com>
2019-09-13 11:38:17 -0300

    Adicionado endpoint para pegar token JWT

--- app/portariaVirtual/urls.py
+++ app/portariaVirtual/urls.py
@@ -6,8 +6,12 @@ from portariaVirtual.schema import schema
 
 from portariaVirtual.accounts import views
 
+#rest framework imports
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', GraphQLView.as_view(schema=schema, graphiql=True)),
     path('hello/', views.HelloView.as_view(), name='hello'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
