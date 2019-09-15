ee46ada8408f8d23ea88a44a2f6f032f9f61d9d3
João Luis Baraky <jlbaraky@gmail.com>
2019-09-14 22:42:29 -0300

    Implementação dos testes das models 'apartment' e 'block'.
    Co-authored-by: Rodrigo Lima <rodrigotiagocl@gmail.com>

--- app/condos/tests.py
+++ app/condos/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
+from condos.models import Apartment, Block
+
 # Create your tests here.
+
+class ApartmentTestCase(TestCase):
+
+    def setUp(self):
+        Block.objects.create(number="1")
+        block = Block.objects.get(number="1")
+        Apartment.objects.create(number="101", block=block)
+
+    def test_apartment_number(self):
+        apartment = Apartment.objects.get(number="101")
+        self.assertEqual(apartment.number, "101")
+
+    def test_apartment_block(self):
+        block = Block.objects.get(number="1")
+        apartment = Apartment.objects.get(block=block)
+
+        self.assertEqual(apartment.block.number, "1")
+
+    def test_apartment_number_max_length(self):
+        apartment = Apartment.objects.get(number="101")
+        max_length = apartment._meta.get_field('number').max_length
+        self.assertEqual(max_length, 6)
+
+class BlockTestCase(TestCase):
+
+    def setUp(self):
+        Block.objects.create(number="1")
+
+    def test_block_number(self):
+        block = Block.objects.get(number="1")
+        self.assertEqual(block.number, "1")
+
+    def test_block_number_max_length(self):
+        block = Block.objects.get(number="1")
+        max_length = block._meta.get_field('number').max_length
+        self.assertEqual(max_length, 4)
