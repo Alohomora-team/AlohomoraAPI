09c499b5ca75d84f4be16dc19a2376df6eb8d36b
bumbleblo <bumbleblo2013@gmail.com>
2019-09-25 02:54:36 -0300

    #71 - Melhorando testes e adicionando atributos de apartamentos e blocos
    Signed-off-by: bumbleblo <bumbleblo2013@gmail.com>

--- app/accounts/tests/test_schema.py
+++ app/accounts/tests/test_schema.py
@@ -3,67 +3,57 @@ from django.test import TestCase
 from graphene.test import Client
 from alohomora.schema import schema
 
-
-# ========== utility function ==========
-
-def create_test_user(**kwargs):
-
-
-    user = get_user_model().objects.create(complete_name='bob o construtor',
-                                           email='charizard@exemplo.com',
-                                           password='1231',
-                                           cpf='12345678910',
-                                           phone='42',
-                                           voice_data='Singing in the Rain',
-                                           admin=True)
-    return user
-
-# ========== createUser mutation tests ==========
-
 class GraphQLTestCase(TestCase):
-    def setUp(self):
-        self._client = Client(schema)
-    def query(self, query: str):
-        resp = self._client.execute(query)
-        return resp
-    def assertNoResponseErrors(self, resp: dict):
-        self.assertNotIn('errors', resp, 'Response has errors')
 
+    def setUp(self):
+        self.user_object = get_user_model()
+        self._client = Client(schema)
+
+    @classmethod
+    def setUpTestData(cls):
+
+        user = get_user_model().objects.create(
+            complete_name='bob o construtor',
+            email='charizard@exemplo.com',
+            password='1231',
+            cpf='12345678910',
+            phone='42',
+            voice_data='Singing in the Rain',
+            admin=True
+        )
+     
     def test_mutation_user(self):
 
-        mutation = '''
-                mutation{
-                  createUser(
-                    completeName: "esquilo-voador",
-                    email: "matpaulo@hoa",
-                    password: "1231234",
-                    cpf: "12345678911",
-                    phone: "11123",
-                    voiceData: "11ok",
-                  ){ user{
-                     completeName
-                     email
-                     cpf
-                     phone
-                     voiceData
-                  }
-                  }
-                }
-        '''
-        response = self.query(query=mutation)
-        self.assertNoResponseErrors(response)
-        self.assertEqual(get_user_model().objects.count(), 1)
-        self.assertEqual(get_user_model().objects.get(id=3).cpf, "12345678911")
-        self.assertEqual(get_user_model().objects.get(id=3).phone, "11123")
-        self.assertEqual(get_user_model().objects.get(id=3).voice_data, "11ok")
-        self.assertEqual(get_user_model().objects.get(id=3).complete_name, "esquilo-voador")
+        mutation = '''mutation {
+                        createUser(
+                            completeName:"felipe",
+                            email: "felipeborges@gmail.com",
+                            password: "1234",
+                            cpf: "1234",
+                            phone: "234123",
+                            block: "1",
+                            apartment : "40",
+                            voiceData: "asdfa"){
+                            user{
+                              completeName
+                              email
+                              cpf
+                              phone
+                              voiceData
+                            }
+                        }
+                      }
+            '''
 
-# ========== createUser query tests ==========
+        response = self._client.execute(mutation)
+        user = self.user_object.objects.get(email='felipeborges@gmail.com')
+        self.assertEqual(user.cpf, "1234")
+        self.assertEqual(user.phone, "234123")
+        self.assertEqual(user.voice_data, "asdfa")
+        self.assertEqual(user.complete_name, "felipe")
 
     def test_query_users(self):
-        user = create_test_user()
 
-        user.save()
         query = '''
         {
             users {
@@ -77,11 +67,10 @@ class GraphQLTestCase(TestCase):
             }
         }
         '''
-        response = self.query(query=query)
-        self.assertNoResponseErrors(response)
+
+        response = self._client.execute(query)
         data = response['data']
         self.assertEqual(len(data), 1)
-        print(data)
         self.assertEqual(data['users'][0]['completeName'], 'bob o construtor')
         self.assertEqual(data['users'][0]['email'], 'charizard@exemplo.com')
         self.assertEqual(data['users'][0]['password'], '1231')
