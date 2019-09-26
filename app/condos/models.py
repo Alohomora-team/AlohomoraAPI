bc4d873453852a88d13272007e76ca78a1960f85
João Luis Baraky <jlbaraky@gmail.com>
2019-09-26 13:15:56 -0300

    Restringindo a criação de um objecto de 'condos' que já existe no banco

--- app/condos/models.py
+++ app/condos/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Block(models.Model):
     number = models.CharField(max_length=4)
 
+    def save(self, *args, **kwargs):
+        if Block.objects.filter(number=self.number).first() is not None:
+            raise Exception('Object already exists')
+
+        super(Block, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.number
 
@@ -11,6 +17,12 @@ class Apartment(models.Model):
 
     block = models.ForeignKey(
         Block, related_name='apartments', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if Apartment.objects.filter(number=self.number, block=self.block).first() is not None:
+            raise Exception('Object already exists')
+
+        super(Apartment, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.number
