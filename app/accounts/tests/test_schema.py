0f69194759cd87f9f83c4fefc538125384d3074c
João Luis Baraky <jlbaraky@gmail.com>
2019-09-27 22:09:06 -0300

    Testes da query de user utilizando cpf e email

--- app/accounts/tests/test_schema.py
+++ app/accounts/tests/test_schema.py
@@ -100,3 +100,34 @@ class GraphQLTestCase(TestCase):
         self.assertEqual(data['users'][0]['cpf'], '12345678910')
         self.assertEqual(data['users'][0]['phone'], '42')
         self.assertEqual(data['users'][0]['voiceData'], 'Singing in the Rain')
+
+    def test_query_user_email(self):
+
+        query = """
+        {
+            user(email: "charizard@exemplo.com"){
+                completeName
+            }
+        }
+
+        """
+
+        response = self._client.execute(query)
+        data = response['data']
+        self.assertEqual(data['user']['completeName'], 'bob o construtor')
+
+    def test_query_user_cpf(self):
+
+        query = """
+        {
+            user(cpf: "12345678910"){
+                completeName
+            }
+        }
+
+        """
+
+        response = self._client.execute(query)
+        data = response['data']
+        self.assertEqual(data['user']['completeName'], 'bob o construtor')
+        
