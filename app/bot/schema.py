6c39251a43259b3b4393f88d5571e5b514b199b7
João Luis Baraky <jlbaraky@gmail.com>
2019-10-30 15:24:26 -0300

    Cria query para 'UserData'.

--- app/bot/schema.py
+++ app/bot/schema.py
@@ -2,17 +2,46 @@ import graphene
 
 from graphene_django.types import DjangoObjectType
 
-from bot.models import Feedback
+from bot.models import Feedback, UserData
 
 class FeedbackType(DjangoObjectType):
     class Meta:
         model = Feedback
 
+class UserDataType(DjangoObjectType):
+    class Meta:
+        model = UserData
+
 class Query():
     all_feedbacks = graphene.List(FeedbackType)
+    all_users_data = graphene.List(UserDataType)
+
+    user_data = graphene.Field(
+            UserDataType,
+            chat_id=graphene.String(),
+            cpf=graphene.String()
+        )
 
     def resolve_all_feedbacks(self, info, **kwargs):
         return Feedback.objects.all()
+
+    def resolve_all_users_data(self, info, **kwargs):
+        return UserData.objects.all()
+
+    def resolve_user_data(self, info, **kwargs):
+        chat_id = kwargs.get('chat_id')
+        cpf = kwargs.get('cpf')
+
+        if chat_id and cpf:
+            return UserData.objects.get(chat_id=chat_id, cpf=cpf)
+
+        if chat_id:
+            return UserData.objects.get(chat_id=chat_id)
+
+        if cpf:
+            return UserData.objects.get(cpf=cpf)
+
+        return None
 
 #Mutations
 
