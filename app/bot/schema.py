c9476a8ac847fb4a061d4ce2ec0f4e733e42d657
João Luis Baraky <jlbaraky@gmail.com>
2019-10-30 15:37:41 -0300

    Cria mutation para 'UserData'.

--- app/bot/schema.py
+++ app/bot/schema.py
@@ -59,5 +59,35 @@ class CreateFeedback(graphene.Mutation):
             comment=feedback.comment
             )
 
+class CreateUserData(graphene.Mutation):
+    chat_id = graphene.String()
+    cpf = graphene.String()
+    user_type = graphene.String()
+
+    class Arguments:
+        chat_id = graphene.String()
+        cpf = graphene.String()
+        user_type = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        chat_id = kwargs.get('chat_id')
+        cpf = kwargs.get('cpf')
+        user_type = kwargs.get('user_type')
+
+        user_data = UserData(
+                chat_id=chat_id,
+                cpf=cpf,
+                user_type=user_type
+            )
+        
+        user_data.save()
+
+        return CreateUserData(
+                chat_id=chat_id,
+                cpf=cpf,
+                user_type=user_data.get_user_type_display()
+            ) 
+
 class Mutation(graphene.ObjectType):
     create_feedback = CreateFeedback.Field()
+    create_user_data = CreateUserData.Field()
