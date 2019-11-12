ae12fca85b76a08dbff2998d246b1d193ae5c1c6
Samuel Borges <samuelmordred@gmail.com>
2019-11-11 23:37:53 -0300

    #22 - adicionando docstrings ao modulo condos/models

--- app/condos/models.py
+++ app/condos/models.py
@@ -1,13 +1,17 @@
+"""Django module for defining Models"""
 from django.db import models
 
 class Block(models.Model):
+    """Block model definition"""
     number = models.CharField(max_length=4, primary_key=True)
 
     def __str__(self):
         return self.number
 
 class Apartment(models.Model):
+    """Apartment class definition"""
     class Meta:
+        """class metadata"""
         unique_together = ['number', 'block']
 
     number = models.CharField(max_length=6)
