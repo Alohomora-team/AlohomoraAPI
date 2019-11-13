d389be5603e72a29cb40debf7110ecc0e7140a8f
Victor Jorge da Silva Gonçalves <victor.eng.unb@gmail.com>
2019-11-13 12:07:43 -0300

    Adicao das funcoes utilitarias para linearizacao e deslinearizacao de matrizes de MFCC
    Co-authored-by: Paulo Batista <higton@users.noreply.github.com>

--- app/accounts/utility.py
+++ app/accounts/utility.py
@@ -37,3 +37,38 @@ def compute_dtw_distance(serie1, serie2):
     distance, _ignored = fastdtw(serie1, serie2)
 
     return distance
+
+def mfcc_matrix_to_array(mfcc_matrix):
+    '''
+    Transform a mfcc_matrix into a mfcc_array.
+
+    mfcc_array will be an array such that rows is abstracted in columns,
+    i.e. mfcc_matrix:[[1,2,3,4,5,6,7,8,9,10,11,12,13], [14,15,16,17,18,19,20,21,22,23,24,25,26]]
+         mfcc_array: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],
+
+    :param mfcc_matrix: the mfcc data matrix
+    :returns: a list "mfcc_array"
+    '''
+
+    mfcc_array = list()
+    for line in mfcc_matrix:
+        mfcc_array += list(line)
+
+    return mfcc_array
+
+def mfcc_array_to_matrix(mfcc_array):
+    '''
+    Transform a mfcc_array into a mfcc_matrix.
+
+    mfcc_array should be an array such that rows is abstracted in columns,
+    i.e. mfcc_array: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],
+         mfcc_matrix:[[1,2,3,4,5,6,7,8,9,10,11,12,13], [14,15,16,17,18,19,20,21,22,23,24,25,26]]
+
+    :param mfcc_array: the mfcc data array
+    :returns: a list of lists "mfcc_matrix"
+    '''
+    column_length, array_len = 13, len(mfcc_array)
+    mfcc_matrix = [mfcc_array[x:x + column_length] for x in range(0, array_len, column_length)]
+
+    return mfcc_matrix
+        
