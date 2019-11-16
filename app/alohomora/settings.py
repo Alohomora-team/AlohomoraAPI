bfc3438e11f7e50de06fe511d3842fc3a5b1d685
Samuel Borges <samuelmordred@gmail.com>
2019-11-16 00:54:43 -0300

    consertando conexao deploy

--- app/alohomora/settings.py
+++ app/alohomora/settings.py
@@ -97,10 +97,7 @@ WSGI_APPLICATION = 'alohomora.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-if (os.environ['DEPLOY'] == True):
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-else:
+if os.environ['DEPLOY'] == False:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
@@ -111,6 +108,9 @@ else:
             'PORT': '5432',
         }
     }
+
+elif os.environ['DEPLOY'] == True:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 
