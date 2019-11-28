1c4d5b75e86d35e429e5b20f5f6540cc9afb5528
VictorJorgeFGA <victor.eng.unb@gmail.com>
2019-11-28 02:34:45 -0200

    Adicionando a query para verificação do volume do audio

---
+++ app/accounts/queries/audio.py
@@ -0,0 +1,31 @@
+'''
+Module used to implement querys to verify audio features
+'''
+import numpy
+import graphene
+import accounts.utility as Utility
+
+class AudioQuery(graphene.AbstractType):
+    '''
+    Class used to implement querys to verify audio features
+    '''
+    audio_has_good_volume = graphene.Boolean(
+        audio_data=graphene.List(graphene.Float, required=True),
+        audio_samplerate=graphene.Int(required=True)
+    )
+
+    def resolve_audio_has_good_volume(self, info, **kwargs):
+        '''
+        Find out if the audio has a good volume
+        '''
+        print('Entrei')
+
+        audio_data = kwargs.get('audio_data')
+        audio_samplerate = kwargs.get('audio_samplerate')
+        audio_data = numpy.array(audio_data)
+
+        audio_data = Utility.treat_audio_data(audio_data, audio_samplerate)
+
+
+        return len(audio_data) / audio_samplerate > 0.3
+            
